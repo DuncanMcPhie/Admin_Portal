12dda71267fcdbe3a0ffde1c53447fd89a00cdd2
Duncan McPhie <47937277+DuncanMcPhie@users.noreply.github.com>
2019-12-04 00:39:45 -0600

    Added Unit Test

---
+++ Admin_Portal_Unit_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Admin_Portal_Unit_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Admin_Portal_Unit_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a39f7314-0941-4f55-9f97-fc589dae4840")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Admin_Portal_Unit_Test/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Admin_Portal;
+using System.Web;
+using System.Web.Mvc;
+using Admin_Portal.Controllers;
+using Admin_Portal.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        //[TestMethod]
+        //public void TestSuperAdminLogin()
+
+        //{
+        //    var userInfo = new LoginAttempt();
+        //    userInfo.Email = "superadmin@email.com";
+        //    userInfo.Password = "super";
+        //    var attemptLogin = new LoginController();
+        //    var result = (RedirectToRouteResult) attemptLogin.Auth(userInfo);
+        //    Assert.AreEqual("Home", result.RouteValues["action"]);
+
+        //}
+
+        [TestMethod]
+        public void TestSignOut()
+        {
+            LoginAttempt userInfo = new LoginAttempt();
+            userInfo.Email = "superadmin@email.com";
+            userInfo.Password = "super";
+            LoginController attemptLogout = new LoginController();
+            attemptLogout.Auth(userInfo);
+            var result = (RedirectToRouteResult)attemptLogout.SignOut();
+            Assert.AreEqual("Home", result.RouteValues["action"]);
+
+        }
+    }
+}
